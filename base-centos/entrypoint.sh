e6a970ec10edda8d8a106dbdbaa1e5b9f426640e
skiinder <skiinder@live.cn>
2023-03-08 09:41:55 +0800

    修正基础镜像

--- base-centos/entrypoint.sh
+++ base-centos/entrypoint.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+set -ex
+
 # 启动sshd
 /sbin/sshd
 
+# 执行初始化脚本
+if ! [ -e /root/.init/READY ]; then
+  for shc in /root/.init/*.sh; do
+    [[ -e "${shc}" ]] || break
+    bash "${shc}"
+  done
+  touch /root/.init/READY
+fi
+
 # 启动ttyd
-/bin/ttyd login
+/bin/ttyd su - atguigu
