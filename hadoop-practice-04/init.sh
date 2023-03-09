a98e57fd80c297fbcfe2d9ed2b007ab78dd3c221
skiinder <skiinder@live.cn>
2023-03-09 11:16:42 +0800

    bug fix

--- hadoop-practice-04/init.sh
+++ hadoop-practice-04/init.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-# 等待其他peer准备完毕
-while ! nc -z hadoop103 22; do
-  echo '等待hadoop103准备就绪'
-  sleep 1
-done
-while ! nc -z hadoop104 22; do
-  echo '等待hadoop104准备就绪'
-  sleep 1
-done
-su - atguigu -c "
+set -ex
+if [ "$HOSTNAME" = "hadoop102" ]; then
+  # 等待其他peer准备完毕
+  while ! nc -z hadoop103 22; do
+    echo '等待hadoop103准备就绪'
+    sleep 1
+  done
+  while ! nc -z hadoop104 22; do
+    echo '等待hadoop104准备就绪'
+    sleep 1
+  done
+  su - atguigu -c "
 start-dfs.sh
 stop-dfs.sh
 xsync /opt/module/hadoop
 "
+fi
