12e5549d79922fe0c4c4205b8c23cb84dd4ee9c1
skiinder <skiinder@live.cn>
2023-03-10 14:15:22 +0800

    linux practice 02

---
+++ linux-practice-02/env.sh
@@ -0,0 +1,8 @@
+function question() {
+  clear
+  cat <<'EOF'
+执行以下操作：
+1. 将/opt/module目录下的apache-zookeeper-3.7.1-bin目录及其子文件的所有者和所属组改为 atguigu:atguigu, 并将其改名为zookeeper
+2. 将zookeeper文件夹重新打包为zookeeper.tar.gz。
+EOF
+}
