ebf85301a4ba74b8dfddd06779f9ef956d2994de
skiinder <skiinder@live.cn>
2023-03-28 16:58:33 +0800

    修正初始化错误

--- flume-practice-02/init.sh
+++ flume-practice-02/init.sh
@@ -1,4 +1,16 @@
-su - atguigu -c "
+#!/bin/bash
+set -ex
+if [ "$HOSTNAME" = "hadoop102" ]; then
+  # 等待其他peer准备完毕
+  while ! nc -z hadoop103 22; do
+    echo '等待hadoop103准备就绪'
+    sleep 1
+  done
+  while ! nc -z hadoop104 22; do
+    echo '等待hadoop104准备就绪'
+    sleep 1
+  done
+  su - atguigu -c "
 zk.sh start
 sleep 1
 kf.sh start
@@ -6,3 +18,4 @@ sleep 2
 kafka-topics.sh --bootstrap-server hadoop102:9092 --topic topic-source --create --partitions 3 --replication-factor 1
 kafka-producer-perf-test.sh --num-records 10000000 --record-size 1024 --producer-props bootstrap.servers=hadoop102:9092 --throughput -1 --topic topic-source
 "
+fi
--- hadoop-practice-06/init.sh
+++ hadoop-practice-06/init.sh
@@ -1,4 +1,16 @@
-su - atguigu -c "
+#!/bin/bash
+set -ex
+if [ "$HOSTNAME" = "hadoop102" ]; then
+  # 等待其他peer准备完毕
+  while ! nc -z hadoop103 22; do
+    echo '等待hadoop103准备就绪'
+    sleep 1
+  done
+  while ! nc -z hadoop104 22; do
+    echo '等待hadoop104准备就绪'
+    sleep 1
+  done
+  su - atguigu -c "
 start-dfs.sh
 mkdir -p test
 
@@ -11,3 +23,4 @@ xcall killall -9 java
 rm -rf test
 xcall 'find  /opt/module/hadoop/data/dfs/data/current -type f | grep \"subdir0\" | xargs -rn5 rm'
 "
+fi
