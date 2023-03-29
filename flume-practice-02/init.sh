c0d28b53ee5ee7739a31622332d282ce15e05cea
skiinder <skiinder@live.cn>
2023-03-29 08:24:16 +0800

    flume practice 06 bug fix

--- flume-practice-02/init.sh
+++ flume-practice-02/init.sh
@@ -16,6 +16,6 @@ sleep 1
 kf.sh start
 sleep 2
 kafka-topics.sh --bootstrap-server hadoop102:9092 --topic topic-source --create --partitions 3 --replication-factor 1
-kafka-producer-perf-test.sh --num-records 10000000 --record-size 1024 --producer-props bootstrap.servers=hadoop102:9092 --throughput -1 --topic topic-source
+kafka-producer-perf-test.sh --num-records 1000000 --record-size 1024 --producer-props bootstrap.servers=hadoop102:9092 --throughput -1 --topic topic-source
 "
 fi
